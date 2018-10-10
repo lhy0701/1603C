b37247019c0a4ed9fcf76638d83aa91ccb25997e
jason <342690199@qq.com>
2018-10-10 16:27:18 +0800

    initial commit

---
+++ Project1/official/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+import {getBrandList, getMakeList} from './api/index.js';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+      getBrandList().then(res=>{
+          console.log('res...', res);
+      });
+      getMakeList(9).then(res=>{
+          console.log('makeList...', res);
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
