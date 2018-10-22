fa3053bb7b73d1e92e05d8d7af0af7ef43cab60b
jason <342690199@qq.com>
2018-10-22 15:06:59 +0800

    update

--- Project1/official/server/index.js
+++ Project1/official/server/index.js
@@ -26,26 +26,45 @@ app.all('*', function(req, res, next) {
 app.get('/', (req, res) => {
     res.send('hello world');
 })
+
+// 发送短信验证码
 app.post('/sendSMS', bodyParser.json(), async (req, res) => {
     console.log('req.body...', req.body);
-    let result = await sendSMS(req.body.phone);
-    if (result){
-        res.json({
-            code: 0,
-            msg: '发送短信成功'
+    try{
+        let {result, code} = await sendSMS(req.body.phone);
+        if (result == -1){
+            res.json({
+                code: -1,
+                msg: '发送短信验证码失败'
+            })
+        }
+        // 把code存入数据库中
+        connection.query(`insert into phone_code (phone, code, create_time) values(${req.body.phone}, ${code}, ${+ new Date()})`, function (error, results, fields) {
+            console.log('result...', results);
+            if (results.insertId){
+                res.json({
+                    code: 1,
+                    msg: '发送短信成功'
+                })
+            }else{
+                res.json({
+                    code: -2,
+                    msg: '短信验证码存入数据库失败'
+                })
+            }
         })
-    }else{
+    }catch{
         res.json({
             code: -1,
-            msg: '发送短信失败'
+            msg: '发送短信验证码失败'
         })
     }
 });
 
 // 登陆接口
 app.post('/login', bodyParser.json(),  (req, res)=>{
-    let {username,password,phone} = req.body;
-    console.log('phone...', phone);
+    let {username,password,phone, code} = req.body;
+    console.log('req.body...', req.body);
     // 查询手机号是否注册过
     connection.query(`select count(*) as num from user where phone=${phone}`, function (error, results, fields) {
         if (error) throw error;
@@ -54,18 +73,34 @@ app.post('/login', bodyParser.json(),  (req, res)=>{
         console.log('result...', results);
         if (results[0].num){
             // 查询到数据，做登陆操作
-            connection.query(`select count(*) as num from user where username="${username}" and password="${password}" and phone=${phone}`, function (error, results, fields) {
-                if (results[0].num){
+            connection.query(`select phone_code.code as code, phone_code.status as status, phone_code.create_time as create_time from user,phone_code where user.phone=phone_code.phone
+            and username="${username}" and password="${password}" and phone_code.phone=${phone} order by phone_code.create_time desc limit 1`, function (error, results, fields) {
+                if (results[0].status == 0){
+                    res.json({
+                        code: -1,
+                        data: {},
+                        msg: '登陆失败，验证码已失效'
+                    })
+                }else if((+new Date()) - results[0].create_time > 60*60*1000) {
+                    res.json({
+                        code: -2,
+                        data: {},
+                        msg: '登陆失败，请重新获取验证码'
+                    })
+                }else if(results[0].code != code){
+                    res.json({
+                        code: -3,
+                        data: {},
+                        msg: '登陆失败，验证码错误'
+                    })
+                }else{
+                    connection.query(`update phone_code set status=0 where phone=${phone}`, function (error, results, fields) {
+
+                    })
                     res.json({
                         code: 1,
                         data: {},
                         msg: '登陆成功'
-                    })
-                }else{
-                    res.json({
-                        code: -2,
-                        data: {},
-                        msg: '用户名或者密码错误'
                     })
                 }
             })
@@ -100,6 +135,8 @@ function sendSMS(phone) {
         accessKeyId,
         secretAccessKey
     })
+    // 随机生成的验证码
+    let code = Math.floor(Math.random()*899999)+100000;
     //发送短信
     return smsClient.sendSMS({
         PhoneNumbers: phone,
@@ -110,7 +147,7 @@ function sendSMS(phone) {
         // 必填: 短信签名 - 可在短信控制台中找到
         TemplateCode: 'SMS_144457028',
         // 必填: 短信模板 - 可在短信控制台中找到， 发送国际 / 港澳台消息时， 请使用国际 / 港澳台短信模版
-        TemplateParam: `{"code":${Math.floor(Math.random()*899999)+100000}}`
+        TemplateParam: `{"code":${code}}`
         // 可选: 模板中的变量替换JSON串,
         // 如模板内容为 "亲爱的${name},您的验证码为${code}" 时。
     }).then(function (res) {
@@ -119,16 +156,20 @@ function sendSMS(phone) {
         } = res
         if (Code === 'OK') {
             //处理返回参数
-            console.log(res)
-            return true;
+            return {
+                result: 1,
+                code
+           }
         }
-        return false;
+        return {
+            result: -1,
+            code
+        };
     }, function (err) {
         console.log(err)
-
     })
 }
 
 app.listen(10001, () => {
-    console.log('正在监听10000端口');
+    console.log('正在监听10001端口');
 });
--- Project1/official/src/views/Login.vue
+++ Project1/official/src/views/Login.vue
@@ -10,8 +10,8 @@
             <el-input type="number" v-model="phone" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="验证码">
-            <el-input type="number" v-model="password" autocomplete="off"></el-input>
-            <el-button type="primary" @click="submitForm()">发送短信验证码</el-button>
+            <el-input type="number" v-model="code" autocomplete="off"></el-input>
+            <el-button type="primary" @click="getSMSCode" :disabled="sending">{{sending?`${time}S`:'发送短信验证码'}}</el-button>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="submitForm()">提交</el-button>
@@ -28,7 +28,10 @@
                 username: '',
                 password: '',
                 phone: '',
-                trl: ''
+                code: '',
+                trl: '',
+                sending: false,
+                time: 60
             }
         },
         methods: {
@@ -45,6 +48,10 @@
                     this.$alert("手机号码有误，请重填");
                     return;
                 }
+                 if(!(/^\d{6}$/.test(this.code))){
+                    this.$alert("验证码有误，请重填");
+                    return;
+                }
                 // window.localStorage.setItem('login', 'true');
                 // this.$router.push('/');
                 fetch('http://169.254.78.172:10001/login', {
@@ -52,7 +59,8 @@
                     body: JSON.stringify({
                         username: this.username,
                         password: md5(this.password+"hello world"),
-                        phone: this.phone
+                        phone: this.phone,
+                        code: this.code
                     }),
                     headers: {
                       'content-type': 'application/json'
@@ -66,6 +74,40 @@
             resetForm(){
                 this.username = '';
                 this.password = '';
+            },
+            getSMSCode(){
+                if (this.sending){
+                    return;
+                }
+                if(!(/^1[34578]\d{9}$/.test(this.phone))){
+                    this.$alert("手机号码有误，请重填");
+                    return;
+                }
+                fetch('http://169.254.78.172:10001/sendSMS', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        phone: this.phone
+                    }),
+                    headers: {
+                      'content-type': 'application/json'
+                    },
+                }).then(res=>res.json())
+                .then(body=>{
+                    if (body.code == 1){
+                        console.log('body...', body);
+                        this.sending = true;
+                        let inter = setInterval(()=>{
+                            this.time--;
+                            if (this.time == 0){
+                                clearInterval(inter);
+                                this.sending = false;
+                                this.time = 60;
+                            }
+                        }, 1000);
+                    }else{
+                        this.$alert(body.msg);
+                    }
+                })
             }
         },
         beforeRouteEnter (to, from, next) {
