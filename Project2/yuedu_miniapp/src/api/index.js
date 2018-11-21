a81fb51a70f945110cbc8ace72051833fefe01b6
jason <342690199@qq.com>
2018-11-21 14:55:28 +0800

    update miniapp

--- Project2/yuedu_miniapp/src/api/index.js
+++ Project2/yuedu_miniapp/src/api/index.js
@@ -1,5 +1,7 @@
 import wepy from 'wepy';
 
+// const host = 'http://123.206.55.50';
+const host = 'http://127.0.0.1';
 function sendRequest(url, data={}, method='GET'){
   return new Promise((resolve, reject)=>{
     wepy.request({
@@ -16,6 +18,22 @@ function sendRequest(url, data={}, method='GET'){
   })
 }
 
+// 获取店铺列表
 export let getShopList = ()=>{
-  return sendRequest('http://127.0.0.1:11111/shop/list');
+  return sendRequest(`${host}:11111/shop/list`);
+}
+
+// 转化坐标
+export let transformCoords = (lat, lon)=>{
+  return sendRequest(`http://api.map.baidu.com/geoconv/v1/?coords=${lon},${lat}&from=1&to=5&ak=kuW8Z5ruzb11LPLuA70gNheiTKkiqao1`)
+}
+
+// 转化区域
+export let transformRegion = (lat, lon)=>{
+  return sendRequest(`http://api.map.baidu.com/geocoder/v2/?callback=cb&location=${lat},${lon}&output=json&pois=1&ak=kuW8Z5ruzb11LPLuA70gNheiTKkiqao1`)
+}
+
+// 登陆接口
+export let login = (username, password)=>{
+  return sendRequest(`${host}:11111/user/login`, {username,password}, 'POST');
 }
